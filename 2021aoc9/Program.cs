f7a19b61be9cb6307e3df9a4ee0e917ff85b2641
Molnár Attila <molnar.attila@szlgbp.hu>
2021-12-16 11:00:13 +0100

    Add project files.

---
+++ 2021aoc9/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021aoc9
+{
+    class Program
+    {
+        class Domborzat
+        {
+
+            int[,] D;
+            int N { get; }
+            int M { get; }
+            bool[,] gödör_e;
+            int speed;
+
+            int[,] szín; // szín[x,y]==z   <=>   "(x,y) pont színe z".
+            List<(int, int)> gödrök; // gödrök[n] == (i,j)    <=>    "az n-edik gödör (i,j) helyen van"
+            List<int> méret; // méret[n] == m    <=>    "az n-edik gödörből kiinduló medence mérete m"
+            public Domborzat(string path, int s = 0)
+            {
+                speed = s;
+                string[] input = System.IO.File.ReadAllLines(path);
+                N = input.Length;
+                M = input.First().Length;
+
+                szín = new int[N, M];
+                gödrök = new List<(int, int)>();
+                méret = new List<int>();
+
+                D = new int[N,M];
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < M; j++)
+                        D[i,j] = int.Parse(input[i][j].ToString());
+                
+                gödör_e = new bool[N, M];
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < M; j++)
+                    {
+                        gödör_e[i, j] = D[i, j] < Szomszédai(i, j).Min(p => D[p.Item1, p.Item2]);
+                        if (gödör_e[i, j])
+                        {
+                            gödrök.Add((i, j));
+                            méret.Add(0);
+                        }
+                    }
+
+            }
+
+            public List<(int,int)> Szomszédai(int x, int y) 
+            {
+                List<(int, int)> szomszédok = new List<(int,int)>();
+                if (0 <= x - 1)
+                    szomszédok.Add((x - 1, y));
+                if (x + 1 < N)
+                    szomszédok.Add((x + 1, y));
+                if (0 <= y - 1)
+                    szomszédok.Add((x, y-1));
+                if (y + 1 < M)
+                    szomszédok.Add((x, y+1));
+                return szomszédok;
+            }
+
+            public int SumRiskLVL()
+            {
+                int s=0;
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < M; j++)
+                        if (gödör_e[i,j])
+                            s += 1 + D[i, j];
+                return s;
+
+            }
+
+            private void MedenceSzínezés()
+            {
+                for (int i = 0; i < gödrök.Count; i++)
+                {
+                    (int x, int y) = gödrök[i];
+                    if (szín[x,y]==0) // fehér
+                        méret[i] = FloodFill(x, y);
+                }
+            }
+
+            public int Három_legnagyobb_medence_szorzata() 
+            {
+                MedenceSzínezés();
+                foreach (var item in méret)
+                {
+                    Console.Write($"{item}, ");
+                }
+                Diagnosztika();
+                Console.WriteLine();
+                return méret.Where(x => x != 0).OrderByDescending(x => x).Take(3).Aggregate(1, (current, item) => current * item);
+            }
+
+            private int FloodFill(int x0, int y0)
+            {
+                int méret = 0;
+                Stack<(int, int)> tennivalók = new Stack<(int, int)>();
+                tennivalók.Push((x0, y0));
+
+                while (0 < tennivalók.Count)
+                {
+                    (int i, int j) = tennivalók.Pop();
+                    méret++;
+                    szín[i, j] = 1; // fekete
+                    foreach ((int,int) szomszéd in Szomszédai(i,j))
+                    {
+                        (int x, int y) = szomszéd;
+                        if (szín[x, y] == 0 && D[x, y] < 9) // fehér
+                        {
+                            tennivalók.Push(szomszéd);
+                            szín[x, y] = -1; // szürke
+                        }
+                    }
+                    //Diagnosztika();
+                }
+                //Diagnosztika();
+                return méret;
+            }
+
+            private void Ki(int n, ConsoleColor szín)
+            {
+                Console.ForegroundColor = szín;
+                Console.Write(n);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            public void Diagnosztika()
+            {
+                Console.Clear();
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++)
+                        switch (szín[i, j])
+                        {
+                            case 1:
+                                Ki(D[i, j], ConsoleColor.Green);
+                                break;
+                            case -1:
+                                Ki(D[i, j], ConsoleColor.Red);
+                                break;
+                            default:
+                                Ki(D[i, j], ConsoleColor.White);
+                                break;
+                        }
+                    Console.WriteLine();
+                }
+                System.Threading.Thread.Sleep(speed);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indulhat?");
+            Console.ReadKey();
+            Domborzat d = new Domborzat("input.txt", 100);
+            Console.WriteLine(d.SumRiskLVL());
+            Console.WriteLine(d.Három_legnagyobb_medence_szorzata());
+            Console.ReadLine();
+        }
+    }
+}
